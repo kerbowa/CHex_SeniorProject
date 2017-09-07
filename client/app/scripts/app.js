060b54ae268ba9037179dba5adf2ce5faeac66d1
Austin Kerbow <amkerbow@gmail.com>
2017-09-07 16:49:31 -0700

    Update URL's to work with HTML5 browsers.

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -19,7 +19,8 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
+      $locationProvider.hashPrefix('');
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
