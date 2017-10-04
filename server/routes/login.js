539a86d39c5dd10d78e7a56d70effdf3e5b440f1
austin <amkerbow@gmail.com>
2017-10-03 21:59:01 -0700

    Update dependencies to enable database.

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res) {
+  // Print posted login form data. 
+  console.log(req.body);
+
+  // Load students from database.
   var sqlite3 = require('sqlite3').verbose();
   var db = new sqlite3.Database('database/chex.db');
 
