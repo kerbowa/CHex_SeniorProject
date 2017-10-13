38903d80cdb127bc712aa71476d7d16947f34a79
DJ <daniel.hayesjr@gmail.com>
2017-10-13 11:44:41 -0700

    Adding view students dashboard

---
+++ client/app/scripts/controllers/students.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:StudentsCtrl
+ * @description
+ * # StudentsCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('StudentsCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
