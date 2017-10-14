9eaa26e257550925f94cf68086c65fd3d2ad979c
Austin Kerbow <amkerbow@gmail.com>
2017-10-14 10:32:47 -0700

    Setup file paths downloads for production.

--- server/routes/gettb.js
+++ server/routes/gettb.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 var hostname = "localhost:3000";
+var app = express();
 
 router.get('/', function(req, res, next) {
 	var sqlite3 = require('sqlite3').verbose();
@@ -14,7 +15,12 @@ router.get('/', function(req, res, next) {
     for (i = 0; i < teamRows.length; i++) {
       // Only generate taskbaords that need to be generated.
       if (teamRows[i].TB_GENERATED) {
-        var filePath = 'http://' + hostname + '/media/Generated Files/' + teamRows[i].NAME + '-sprint-task-board.docx';
+        var filePath = "";
+        if (app.get('env') === 'production')
+          filePath = '/media/Generated Files/' + teamRows[i].NAME + '-sprint-task-board.docx';
+        else
+          filePath = 'http://' + hostname + '/media/Generated Files/' + teamRows[i].NAME + '-sprint-task-board.docx';
+
         allTeams.teams.push({"_id" : i,
                              "name" : teamRows[i].NAME,
                              "filepath" : filePath});
