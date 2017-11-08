b05f8e39c16b6208f8065312af5f1b5cb433308e
DJ <daniel.hayesjr@gmail.com>
2017-11-08 15:07:54 -0800

    Added delete student link and API: :

--- client/app/scripts/controllers/students.js
+++ client/app/scripts/controllers/students.js
@@ -9,19 +9,25 @@
  */
 angular.module('clientApp')
   .controller('StudentsCtrl', ['$scope', '$http', '$window', function ($scope, $http, $window) {
-  	$scope.customers = [
-  		{ name: "DJ Hayes", city: "Sacramento"},
-  		{ name: "Ellie Davidson", city: "Sacramento"},
-  		{ name: "Kyle Ray", city: "West Sacramento"}
-  	]
 
-	var req = $http.get('/api/getstudents');
-	var scope = this;
-	req.then(function (res) {
-		scope.studentNames = res.data.student;
-		console.log(res);
-	});
-	req.catch(function (err) {
-		console.log(err);
-	});
-  }]);
+  var req = $http.get('/api/getstudents');
+  var scope = this;
+  req.then(function (res) {
+    scope.studentNames = res.data.student;
+    console.log(res);
+  });
+  req.catch(function (err) {
+    console.log(err);
+  });
+
+  $scope.deletestudent = function(student_id) {
+    console.log('Delete student cntrl - id = ' + student_id);
+    var data = { id: student_id };
+    var req = $http.post('/api/deletestudent/', data);
+    req.then(function (res) {
+    });
+    req.catch(function (err) {
+      console.log(err);
+    });
+  };
+}]);
--- server/routes/getstudents.js
+++ server/routes/getstudents.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
   };
   db.all('SELECT * FROM STUDENT', function(err, rows) {
     for (i = 0; i < rows.length; i++) {
-      allStudents.student.push({"_id" : i,
+      allStudents.student.push({"_id" : rows[i].STUDENT_ID,
                                "name" : rows[i].NAME,
                                "email" : rows[i].EMAIL,
                                "team" : rows[i].TEAM_ID});
