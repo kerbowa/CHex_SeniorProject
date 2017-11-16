00de9240f6274d9d5a00f14282f0cc9fd99664e4
Ali H. Iqbal <iqbal.ali.7@gmail.com>
2017-11-16 13:36:21 -0800

    create client update

--- client/app/scripts/controllers/clients.js
+++ client/app/scripts/controllers/clients.js
@@ -8,17 +8,16 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('ClientsCtrl', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+  .controller('ClientsCtrl', ['$scope', '$http', '$mdDialog', function ($scope, $http, $mdDialog) {
         
-        // $scope.client_id =null;
-        // $scope.clientname =null;
-        // $scope.clientemail =null;
-        // $scope.clientstatus =null;
+        $scope.client_id =null;
+        $scope.clientname =null;
+        $scope.clientemail =null;
+        $scope.clientstatus =null;
 
-        $scope.reverseOrder = true;
-        $scope.sortField = 'client_id';
-
-  var req = $http.get('/api/getclients');
+   $scope.customFullscreen = false;
+$scope.initFirst = function() {
+    var req = $http.get('/api/getclients');
   var scope = this;
   req.then(function (res) {
     scope.clientNames = res.data.client;
@@ -28,37 +27,45 @@ angular.module('clientApp')
     console.log(err);
   });
 
+}
 
-  $scope.sortBy = function(sortField) {
-      $scope.reverseOrder = ($scope.sortField === sortField) ? !$scope.reverseOrder : false;
-      $scope.sortField = sortField;
-    };
 
-    
-    $scope.addClient = function() {
-      $scope = $scope.$new();
-        var Indata = ({'clientname': $scope.client.name, 'clientemail': $scope.client.email, 'clientstatus': $scope.client.status});
-          console.log(Indata);
-   
-       $http({
-          url: '/api/createclient',
-          method: 'POST',
-          data: Indata,
-          headers: {'Content-Type': 'application/json'}
+      $scope.showCreate = function(ev) {
+        $mdDialog.show({
+          controller: CreateController,
+          templateUrl: 'dialog1.tmpl.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          scope: $scope.$new(), // Uses prototypal inheritance to gain access to parent scope
+          clickOutsideToClose:false,
+          fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
         })
-
-        req.then(function (res) {
-         console.log(res);
-         alert('Client has been added');
-
+        .then(function() {
+        }, function() {
+          $scope.status = 'You cancelled the dialog.';
         });
+      };
 
-        req.catch(function (err) {
-        console.log(err);
-        });
-  };
-
-
+     function CreateController($scope, $mdDialog) {
+       $scope.hide = function() {
+         $mdDialog.hide();
+       };
+       $scope.cancel = function() {
+         $mdDialog.cancel();
+       };
+       $scope.createclient = function() {
+         $scope.statusMsg = 'Sending data to server...';
+         var Indata = {'clientname': $scope.client.name, 'clientemail': $scope.client.email, 'clientstatus': $scope.client.status};
+         $http({
+           url: '/api/createclient',
+           method: 'POST',
+           data: Indata,
+           headers: {'Content-Type': 'application/json'}
+         })
+         $scope.initFirst();
+         $mdDialog.hide();
+       };
+     }
 
 
   $scope.editClient = function() {
@@ -72,5 +79,15 @@ angular.module('clientApp')
   };
 
 
-}] ) ;
+    var req = $http.get('/api/getclients');
+  var scope = this;
+  req.then(function (res) {
+    scope.clientNames = res.data.client;
+    console.log(res);
+  });
+  req.catch(function (err) {
+    console.log(err);
+  });
 
+
+}] ) ;
