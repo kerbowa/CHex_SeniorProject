ec6445122f53881ccf4adbb2cf5649b6efdcbcd9
DJ <daniel.hayesjr@gmail.com>
2017-11-29 14:55:56 -0800

    Added xlsx parsing

--- server/routes/uploadstudents.js
+++ server/routes/uploadstudents.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var path = require('path');
 var multer  = require('multer');
 var hostname = "localhost:9000";
+var XLSX = require('xlsx');
 
 // File upload for bulk add students
 var storage = multer.diskStorage({
@@ -11,12 +12,24 @@ var storage = multer.diskStorage({
         cb(null, 'media/Uploads')
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname+ '-' + Date.now()+'.jpg')
+        cb(null, 'students.xlsx')
     }
 });
 var upload = multer({ storage: storage });
 
 router.post('/', upload.single('file'), function(req, res) {
+	console.log('Begin parse...');
+	try {
+		var workbook = XLSX.readFile('media/Uploads/students.xlsx');
+	}
+	catch(err) {
+		console.log(err);
+	}
+	var sheet1 = workbook.SheetNames[0];
+	var worksheet = workbook.Sheets[sheet1];
+	var jsonStudents = XLSX.utils.sheet_to_json(workbook.Sheets[sheet1]);
+	console.log(jsonStudents);
+	console.log('End parse.');
 });
 
 module.exports = router;
