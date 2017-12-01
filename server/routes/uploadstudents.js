690ffe521fc57afde84724b91fbc61967f6c0ea8
DJ <daniel.hayesjr@gmail.com>
2017-12-01 13:09:33 -0800

    Added bulk student upload db insert

--- server/routes/uploadstudents.js
+++ server/routes/uploadstudents.js
@@ -18,18 +18,33 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage });
 
 router.post('/', upload.single('file'), function(req, res) {
-	console.log('Begin parse...');
-	try {
-		var workbook = XLSX.readFile('media/Uploads/students.xlsx');
-	}
-	catch(err) {
-		console.log(err);
-	}
-	var sheet1 = workbook.SheetNames[0];
-	var worksheet = workbook.Sheets[sheet1];
-	var jsonStudents = XLSX.utils.sheet_to_json(workbook.Sheets[sheet1]);
-	console.log(jsonStudents);
-	console.log('End parse.');
+  console.log('Begin parse...');
+  try {
+    var workbook = XLSX.readFile('media/Uploads/students.xlsx');
+  }
+  catch(err) {
+    console.log(err);
+  }
+  var sheet1 = workbook.SheetNames[0];
+  var worksheet = workbook.Sheets[sheet1];
+  var jsonStudents = XLSX.utils.sheet_to_json(workbook.Sheets[sheet1]);
+  console.log(jsonStudents);
+  console.log('End parse.');
+
+  var sqlite3 = require('sqlite3').verbose();
+  var db = new sqlite3.Database('database/chex.db')
+  jsonStudents.forEach(function(element) {
+    console.log('Insert: ' + element.Student + ' ' + element.Email);
+  
+    db.run(`INSERT INTO STUDENT (NAME, EMAIL) VALUES ('${element.Student}', '${element.Email}')`, function(err) {
+    if (err) {
+      return console.error(err.message);
+    }
+      console.log(`Row(s) added ${this.changes}`);
+    });
+  });
+  db.close();
+  //console.log(`final Row(s) added ${this.changes}`);
 });
 
 module.exports = router;
